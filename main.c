77230a83ca8a51eb3bfb7fb3409da7dc9557c1f0
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-30 12:42:23 +0530

    Update and rename ActivityOne.c to main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+/**
+ * @file ActivityOne.c
+ * @author Maneesh
+ * @brief Activity one of embedded systems
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+#include "activity1.h"
+
+int main(void)
+{
+    led_init();
+    // Insert code
+
+while(1)
+    {
+       if(BUTTON_SENSOR_ON) //Checking if the input bit to 7th bit of pinB is made 0 from 1 by pressing led
+        {
+
+            if(HEATER_ON) //Checking if the input bit to 6th bit of pinB is made 0 from 1 by pressing led
+            {
+                _delay_ms(20);
+                SET_LED; //make 0th bit of port B as 1, makes led glow
+                _delay_ms(20);
+
+            }
+            else
+            {
+                CLEAR_LED; // make led off
+            }
+        }
+        else
+        {
+
+                CLEAR_LED; //make led off
+        }
+    }
+
+ return 0;
+}
