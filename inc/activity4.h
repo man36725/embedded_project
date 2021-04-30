18869184aa1695a5a0f59efcf157d4df2845a75e
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-30 20:03:09 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -10,26 +10,25 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include <avr/io.h>
-#include <util/delay.h>
+
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 /**
- * @brief A macro to check if the data is received into the buffer or not
+ * @brief A macro to check the data is received into the buffer or not
  * 
  */
 #define UART_DATA_NOT_RECEIVED !(UCSR0A & (1<<RXC0))
 
 /**
- * @brief A macro to check if the data is not written into the buffer
+ * @brief A macro to check the data is not written into the buffer
  * 
  */
 #define UART_DATA_NOT_WRITTEN !(UCSR0A & (1<<UDRE0))
 
 /**
- * @brief A program to initialize the UART serial communication
+ * @brief A program to initialize the UART 
  * 
  * @param ubrr_value 
  */
