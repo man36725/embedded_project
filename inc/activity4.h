f3ab127bf6ba6e44196ce01be69eb414a7de6e42
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-30 19:15:26 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -13,6 +13,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include <avr/io.h>
+#include <util/delay.h>
+
 /**
  * @brief A macro to check if the data is received into the buffer or not
  * 
@@ -26,11 +29,21 @@
 #define UART_DATA_NOT_WRITTEN !(UCSR0A & (1<<UDRE0))
 
 /**
+ * @brief A macro to enable the uart and uart interrupts
+ * 
+ */
+#define UART_ENABLED UCSR0B|=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0)
+/**
+ * @brief A macro to define uart character size
+ * 
+ */
+#define UART_CHARACTER_SIZE UCSR0C|=(1<<UCSZ00)|(1<<UCSZ01)
+/**
  * @brief A program to initialize the UART serial communication
  * 
  * @param ubrr_value 
  */
-void UARTinit(uint16_t UBRR_VALUE);
+void UARTinit(uint16_t ubrr_value);
 
 /**
  * @brief A function to read characters coming from other UART port
