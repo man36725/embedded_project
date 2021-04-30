6c80d2b499df57297947da5646dd541cff9529ef
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-30 12:39:11 +0530

    Create activity1.h

---
+++ inc/activity1.h
@@ -0,0 +1,81 @@
+#ifndef __ACTIVITY_1_H__
+#define __ACTIVITY_1_H__
+/**
+ * @file activity1.h
+ * @author Maneesh
+ * @brief Header file for activity1
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+/**
+ * Macros
+ */
+#include <avr/io.h>
+
+/**
+ * @brief A macro to check if the button is pressed or not. If pressed, the input to 1th bit of PIND is 0.
+ *
+ */
+#define BUTTON_SENSOR_ON !(PIND&(1<<PD0))
+
+/**
+ * @brief A macro to check if the heater is pressed or not. If pressed, the input to 0th bit of PIND is 0.
+ *
+ */
+#define HEATER_ON !(PIND&(1<<PD1))
+
+/**
+ * @brief A macro to turn on the LED by setting 0th bit of PORTB
+ *
+ */
+#define SET_LED PORTB|=(1<<PB0)
+
+/**
+ * @brief A macro to turn off the LED by clearing the 0th bit of PORTB
+ *
+ */
+#define CLEAR_LED PORTB&=~(1<<PB0)
+
+/**
+ * @brief A macro to set 0th bit of PORTB as output
+ *
+ */
+#define SET_LED_AS_OUTPUT DDRB|=(1<<PB0)
+
+/**
+ * @brief A macro to set 0th bit of PORTD as input
+ *
+ */
+#define SET_BUTTON_AS_INPUT DDRD&=~(1<<PD0)
+
+/**
+ * @brief A macro to set 1st bit of PORTD as input
+ *
+ */
+#define SET_HEATER_AS_INPUT DDRD&=~(1<<PD1)
+
+/**
+ * @brief A macro to set 0th bit of PORTD
+ *
+ */
+#define PULLUP_BUTTON PORTD|=(1<<PD0)
+
+/**
+ * @brief A macro to set 1st bit of PORTD
+ *
+ */
+#define PULLUP_HEATER PORTD|=(1<<PD1)
+
+/**
+ * @brief A function to operate the button and heater sensor
+ *
+ */
+void led_init();
+
+
+#endif // PORT_H_INCLUDED
