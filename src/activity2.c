9f3dfc81351a0023d8e800286cfd27133fd7bffb
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-30 13:05:48 +0530

    Create activity2.c

---
+++ src/activity2.c
@@ -0,0 +1,36 @@
+/**
+ * @file activity2.c
+ * @author Maneesh
+ * @brief  to get the ADC values 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ */
+#include "activity2.h"
+#include <avr/io.h>
+void Init_ADC(){
+    SET_REFERENCE_VOLTAGE;
+    ADC_ENABLE_PRESCALE;
+
+}
+
+/**
+ * @brief A funciton to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t Read_ADC(uint8_t ch)
+{
+    ADMUX&=0xf8;
+    ch = ch&0b00000111;
+    ADMUX|=ch;
+
+    ADCSRA|=(1<<ADSC);
+
+    while(ADC_INTERRUPT_NOT_OCCURED);
+    SET_ADC_INTERRUPT_FLAG;
+    return(ADC);
+
+}
